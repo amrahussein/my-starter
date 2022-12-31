85b7b7a658eb757d278e4b51293fa221b875e6b4
AbdoAdel <abdo48439@gmail.com>
2022-12-31 17:30:28 +0200

    Add Js and Css Files

--- src/features/calculator/main.js
+++ src/features/calculator/main.js
@@ -1,11 +1,37 @@
 console.log('js');
 
-const btn = document.getElementById('btn');
-console.log('btn: ', btn);
-
-btn.addEventListener('click', () => {
-  alert('hello world');
-});
-
 //
 
+// let enterDigits = (val) => {
+//   console.log('Hello');
+//   document.getElementById('result').value += val;
+//   return val;
+// };
+
+let digits = document.getElementById('digits');
+digits.addEventListener('click', (e) => {
+  console.log(e.target);
+  console.log(e.target.value);
+  console.log(e.currentTarget);
+  let currentDigit = e.target.value;
+  document.getElementById('result').value += currentDigit;
+});
+
+//sum
+let sum = () => {
+  // console.log('Hello');
+  let x = document.getElementById('result').value;
+  let y = eval(x);
+  let result = (document.getElementById('result').value = y);
+  //   console.log(y);
+  return result;
+};
+
+let clearScreen = () => {
+  // console.log('Hello');
+  document.getElementById('result').value = '';
+};
+
+// window.enterDigits = enterDigits;
+window.sum = sum;
+window.clearScreen = clearScreen;
