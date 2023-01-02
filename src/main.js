0c9b2cbbd359d5a514f513cb1b502cdfeedc5e22
Amro Abdelkamel <amromoorie@gmail.com>
2023-01-02 23:58:35 +0200

    refactor (main js)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-// ******************
-// Import Main Styles
-// ******************
-import './style.scss';
-
+// selectors
+const root = document.documentElement;
+const navigation = document.getElementById('nav');
 // Wait  HTML document to be completely parsed
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.documentElement;
-
-  // const main = document.getElementById('main');
-
   const routes = {
-    './calculator': async () => {
+    '/': async () => {
+      // load landing page
+      let response = await fetch('./index.html');
+      let html = await response.text();
+      root.innerHTML = html;
+    },
+    '/calculator': async () => {
       // load calculator page
       let response = await fetch('/features/calculator/index.html');
       let html = await response.text();
@@ -19,15 +19,22 @@ document.addEventListener('DOMContentLoaded', () => {
       await import('./features/calculator/main');
     },
 
-    './password-generator': async () => {
+    '/password-generator': async () => {
       // load password-generator page
       let response = await fetch('/features/password-generator/index.html');
       let html = await response.text();
       root.innerHTML = html;
-      await import('./features/password-generator/style.scss');
-      await import('./features/password-generator/main');
+      try {
+        await import('./features/password-generator/style.scss');
+        await import('./features/password-generator/main');
+      } catch (error) {
+        console.error(
+          'Error happened during fetching a password-generator html: ',
+          error,
+        );
+      }
     },
-    './currency-converter': async () => {
+    '/currency-converter': async () => {
       // load currency-converter page
       let response = await fetch('/features/currency-converter/index.html');
       let html = await response.text();
@@ -35,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
       await import('./features/currency-converter/style.scss');
       await import('./features/currency-converter/main');
     },
-    './unit-converter': async () => {
+    '/unit-converter': async () => {
       // load unit-converter page
       let response = await fetch('/features/unit-converter/index.html');
       let html = await response.text();
@@ -45,25 +52,24 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   };
 
-  window.onpopstate = () => {
-    // get the current URL
-    const currentUrl = window.location.pathname;
-    // invoke the route handler for the current URL
-    routes[currentUrl]();
-  };
+  // Event Listeners
+  // handle navigation
+  navigation.addEventListener('click', handleNavigation);
 
-  const navigation = document.getElementById('nav');
-  console.log('navigation: ', navigation);
+  // respond to current URL state
+  window.addEventListener('popstate', handleCurrentUrlState);
 
-  navigation.addEventListener('click', (event) => {
+  // function handlers
+  function handleNavigation(event) {
     event.preventDefault();
     // get the clicked link's URL
     const clickedUrl = event.target.getAttribute('href');
+    console.log('clickedUrl: ', clickedUrl);
 
     // push the clicked URL to the browser history
-    window.history.pushState({}, '', clickedUrl);
-    // // invoke the route handler for the clicked URL
-    // routes[clickedUrl]()
+    history.pushState({ clickedUrl }, '', clickedUrl);
+    console.log('window.history: ', history);
+
     // check if the routes object has a property with the key equal to the clickedUrl value
     if (routes.hasOwnProperty(clickedUrl)) {
       // invoke the route handler for the clicked URL
@@ -71,6 +77,15 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
     // handle the case where there is no matching route
-    console.error('Paged Not Found');
-  });
+    console.error('Page Not Found');
+  }
+
+  function handleCurrentUrlState() {
+    // get the current URL
+    const currentUrl = window.location.pathname;
+    console.log('currentUrl: ', currentUrl);
+    // invoke the route handler for the current state URL
+    // routes[currentUrl]();
+    routes[currentUrl]();
+  }
 });
